ef8f40b2b6c3518f0753fc57944701125cee8b05
JonathanDelaney <71841102+JonathanDelaney@users.noreply.github.com>
2023-03-09 17:04:11 +0100

    Update config.js
    add live prefix

--- AdyenCheckoutsJS-main_2/server/node/utils/config.js
+++ AdyenCheckoutsJS-main_2/server/node/utils/config.js
@@ -1,7 +1,7 @@
 const { CHECKOUT_APIKEY, MERCHANT_ACCOUNT, CLIENT_KEY } = process.env;
 
-const API_VERSION = 'v69';
-const CHECKOUT_URL = `https://checkout-test.adyen.com/${API_VERSION}`;
+const API_VERSION = 'v70';
+const CHECKOUT_URL = `https://14bc048714e340cf-AdyenTechSupport-checkout-live.adyenpayments.com/checkout/${API_VERSION}`;
 
 module.exports = {
     CHECKOUT_APIKEY,
