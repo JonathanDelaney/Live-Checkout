40da7f9a6b42596edd78f8e8f95563271f8b4bf3
JonathanDelaney <71841102+JonathanDelaney@users.noreply.github.com>
2023-03-10 15:20:02 +0100

    Update applepay.js
    update configuration

--- AdyenCheckoutsJS-main_2/src/applepaySessions/applepay.js
+++ AdyenCheckoutsJS-main_2/src/applepaySessions/applepay.js
@@ -1,31 +1,39 @@
-sessionsDropin().then((session) => {
-  initSession();
-  async function initSession() {
-    const checkout = await AdyenCheckout({
-      clientKey: "test_M35ZRWIW6JHMPOLIAJELF2OYEYIKZQEP",
-      environment: "test",
-      session,
-      onChange: (state, component) => {
-        updateStateContainer(state); // Demo purposes only
-      },
-      onPaymentCompleted: (result, component) => {
-        console.info(result, component);
-        updateResponseContainer(result);
-      },
-      onError: (error, component) => {
-        console.error(error.name, error.message, error.stack, component);
-        updateResponseContainer(response.message);
-      },
-    });
-    const applePayComponent = checkout.create("applepay");
-
-    applePayComponent
-      .isAvailable()
-      .then(() => {
-        applePayComponent.mount("#applepay-container");
-      })
-      .catch((e) => {
-        console.log(e);
+getClientKey().then((clientKey) => {
+  sessionsDropin().then((session) => {
+    initSession();
+    async function initSession() {
+      const checkout = await AdyenCheckout({
+        clientKey: clientKey,
+        environment: "live",
+        session,
+        onChange: (state, component) => {
+          updateStateContainer(state); // Demo purposes only
+        },
+        onPaymentCompleted: (result, component) => {
+          console.info(result, component);
+          updateResponseContainer(result);
+        },
+        onError: (error, component) => {
+          console.error(error.name, error.message, error.stack, component);
+          updateResponseContainer(response.message);
+        },
       });
-  }
+      const applePayComponent = checkout.create("applepay",{
+        countryCode: "NL",
+        amount: {
+          currency: "EUR",
+          value: 100
+        }
+      });
+
+      applePayComponent
+        .isAvailable()
+        .then(() => {
+          applePayComponent.mount("#applepay-container");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  });
 });
