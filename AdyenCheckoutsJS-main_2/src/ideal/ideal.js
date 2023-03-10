a6f82ef8dc55987830c392dbf3b32f22b0cfe047
JonathanDelaney <71841102+JonathanDelaney@users.noreply.github.com>
2023-03-10 15:35:47 +0100

    Update ideal.js
    update environment

--- AdyenCheckoutsJS-main_2/src/ideal/ideal.js
+++ AdyenCheckoutsJS-main_2/src/ideal/ideal.js
@@ -9,7 +9,7 @@ getClientKey().then((clientKey) => {
 
     // 1. Create an instance of AdyenCheckout
       const checkout = await AdyenCheckout({
-          environment: 'test',
+          environment: 'live',
           locale: "en-US",
           clientKey: clientKey, // Mandatory. clientKey from Customer Area
           paymentMethodsResponse,
@@ -50,4 +50,42 @@ getClientKey().then((clientKey) => {
           showPayButton: true
       }).mount('#card-container');
   })
+   
+  // Redirect handling code starts here till the end
+
+  const getSearchParameters = (search = window.location.search) =>
+  search
+    .replace(/\?/g, "")
+    .split("&")
+    .reduce((acc, cur) => {
+      const [key, prop = ""] = cur.split("=");
+      acc[key] = decodeURIComponent(prop);
+      return acc;
+    }, {});
+
+  const { redirectResult } = getSearchParameters(window.location.search);
+
+  async function handleRedirectResult(redirectResult) {
+    const checkout = await AdyenCheckout({
+      environment: "live",
+      clientKey: clientKey
+    });
+    const dropin = checkout
+      .create("dropin", {
+        setStatusAutomatically: false,
+      })
+      .mount("#dropin-container");
+
+    const response = await submitDetails({ details: { redirectResult } });
+    if (response.resultCode === "Authorised") {
+      dropin.setStatus("success", { message: "Payment successful!" });
+    } else if (response.resultCode !== "Authorised") {
+      dropin.setStatus("error", { message: "Oops, try again please!" });
+    }
+  }
+
+
+  if (redirectResult) {
+    handleRedirectResult(redirectResult);
+  }
 });
