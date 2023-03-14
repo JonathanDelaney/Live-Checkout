5e361068e7d412e1fd574e3cc7a9db367d6cb1fb
JonathanDelaney <71841102+JonathanDelaney@users.noreply.github.com>
2023-03-14 10:13:10 +0100

    Update paymentDetails.js
    log params

--- AdyenCheckoutsJS-main_2/server/node/api/paymentDetails.js
+++ AdyenCheckoutsJS-main_2/server/node/api/paymentDetails.js
@@ -5,7 +5,7 @@ const handleCallback = require("../utils/handleCallback");
 module.exports = (res, request) => {
   console.log("Request being sent: "
   const params = getPostParameters("/payments/details", request);
-
+  console.log("Params: ", params);
   post(params, (err, response, body) =>
     handleCallback({ err, response, body }, res)
   );
