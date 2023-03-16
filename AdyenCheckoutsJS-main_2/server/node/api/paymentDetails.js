dbb88824262cc31f7843f57fe2cff58169454bf1
JonathanDelaney <71841102+JonathanDelaney@users.noreply.github.com>
2023-03-16 12:10:13 +0100

    Update paymentDetails.js
    remove url

--- AdyenCheckoutsJS-main_2/server/node/api/paymentDetails.js
+++ AdyenCheckoutsJS-main_2/server/node/api/paymentDetails.js
@@ -3,7 +3,7 @@ const getPostParameters = require("../utils/getPostParameters");
 const handleCallback = require("../utils/handleCallback");
 
 module.exports = (res, request) => {
-  const params = getPostParameters("https://14bc048714e340cf-AdyenTechSupport-checkout-live.adyenpayments.com/checkout/payments/details", request);
+  const params = getPostParameters("/payments/details", request);
   post(params, (err, response, body) =>
     handleCallback({ err, response, body }, res)
   );
