396b9702987418cc3a4fbe9aafbabc8e9996e786
JonathanDelaney <71841102+JonathanDelaney@users.noreply.github.com>
2024-04-04 13:47:50 +0200

    Update config.js

--- AdyenCheckoutsJS-main_2/server/node/utils/config.js
+++ AdyenCheckoutsJS-main_2/server/node/utils/config.js
@@ -1,6 +1,6 @@
 const { CHECKOUT_APIKEY, MERCHANT_ACCOUNT, CLIENT_KEY } = process.env;
 
-const API_VERSION = 'v70';
+const API_VERSION = 'v68';
 const CHECKOUT_URL = `https://14bc048714e340cf-AdyenTechSupport-checkout-live.adyenpayments.com/checkout/${API_VERSION}`;
 
 module.exports = {
